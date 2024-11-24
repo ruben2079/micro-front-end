e7e963ce58ce03a486a627d8bab9fe149d17808b
Ruben Guzman <pryices@gmail.com>
2024-11-24 14:11:00 -0500

    trying to settup microfrontend with module federation

--- homepage/src/app/app.component.ts
+++ homepage/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppRoutingModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
